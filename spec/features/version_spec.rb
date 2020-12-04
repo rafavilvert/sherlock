1b692b71dab60cc8172c17bbb19c1500b17aa06b
robertvilvert <robert.vilvert@resultadosdigitais.com.br>
2020-12-04 11:23:07 -0300

    Improve spec description

--- spec/features/version_spec.rb
+++ spec/features/version_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 require 'json'
 
 RSpec.describe VersionController, type: :controller do
-  describe "GET Version" do
+  describe "GET App Version" do
     it "returns a 200" do
       get :index
       expect(response).to have_http_status(:ok)
